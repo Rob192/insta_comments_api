922d2524869b7cbe025fc46d8435407596f16ec3
Rob192 <reynaud.robin@gmail.com>
2020-07-29 19:40:33 +0200

    uncomment treatment for sentence

--- app/main.py
+++ app/main.py
@@ -1,11 +1,11 @@
 from fastapi import FastAPI
-#from app.src.find_comment import CommentFinder
+from app.src.find_comment import CommentFinder
 
 app = FastAPI()
 
-# commentFinder = CommentFinder()
+commentFinder = CommentFinder()
 
 @app.get("/")
 async def give_sentence(sentence: str):
-    comment = sentence #commentFinder.find_comment(sentence)
+    comment = commentFinder.find_comment(sentence)
     return {"comment": comment}
